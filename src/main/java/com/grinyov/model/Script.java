ce17655cbd77e83862b8e14886a04e171ea791af
Vitaliy Grinyov <vgrinyov@gmail.com>
2017-06-17 01:08:10 +0300

    add model

---
+++ src/main/java/com/grinyov/model/Script.java
@@ -0,0 +1,14 @@
+package com.grinyov.model;
+
+import lombok.Data;
+
+/**
+ * Created by vgrinyov.
+ */
+@Data
+public class Script {
+    private String id;
+    private String script;
+    private String status;
+    private String result;
+}
