317d4e4a46459bc1104d0917d7f9e4ae39414699
grinyov <vgrinyov@gmail.com>
2017-07-08 03:00:18 +0300

    add script compile validator

---
+++ src/main/java/com/grinyov/util/ScriptValidator.java
@@ -0,0 +1,59 @@
+package com.grinyov.util;
+
+import com.grinyov.exception.FailedScriptCompilationException;
+import com.grinyov.model.Script;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import javax.script.Compilable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * Created by vgrinyov on 08.07.17.
+ */
+public class ScriptValidator implements Validator {
+
+    private static final Logger logger = Logger.getLogger(ScriptValidator.class);
+
+    @Value("${engine.name}")
+    private String engineName;
+
+    public ScriptEngine getEngine() {
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName(engineName);
+        logger.debug("Engine was created");
+        return engine;
+    }
+
+    public boolean compileScript(String script, ScriptEngine engine) {
+        try {
+            ((Compilable) engine).compile(script);
+            logger.debug("Script compiled successful. :-) \n");
+            return true;
+        } catch (ScriptException e) {
+            logger.warn("Script \"" + script + "\" compiled unsuccessful. :-(");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Script.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        final Script script = (Script) target;
+        ScriptEngine engine = getEngine();
+
+        if (!compileScript(script.getScript(), engine)) {
+            logger.error("The script can not compile");
+            errors.rejectValue("script", "Script.script.compile.failed", "Еhe script did not compile");
+            throw new FailedScriptCompilationException("script compiled unsuccessful!");
+        }
+    }
+}
