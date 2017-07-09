23f585672cc1b7c1241cb39744a2f6d2f34040f2
grinyov <vgrinyov@gmail.com>
2017-07-10 01:30:59 +0300

    add support spring restdocs and binding it with running tests

--- src/test/java/com/grinyov/controller/ScriptResourceControllerTest.java
+++ src/test/java/com/grinyov/controller/ScriptResourceControllerTest.java
@@ -6,15 +6,39 @@ import com.grinyov.model.Script;
 import com.grinyov.service.ScriptProccessingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.halLinks;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -23,9 +47,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ScriptengineApiApplication.class)
-@WebAppConfiguration
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs("build/generated-snippets")
 public class ScriptResourceControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
@@ -34,6 +60,46 @@ public class ScriptResourceControllerTest {
     @MockBean
     private ScriptRepository scriptRepository;
 
+
+     @Test
+    public void createScript() throws Exception {
+        Script script = script();
+        String requestBody = saveRequestJsonString(script);
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .post("/scripts")
+                .accept(MediaTypes.HAL_JSON)
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        final Script createdScript = findCreatedScript();
+        resultActions.andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, "http://localhost:8080/scripts/" + createdScript.getId()))
+                .andExpect(jsonPath("$.id", is(createdScript.getId().intValue())))
+                .andExpect(jsonPath("$.script", is(createdScript.getScript())))
+                .andExpect(jsonPath("$.status", is(createdScript.getStatus())))
+                .andExpect(jsonPath("$.result", is(createdScript.getResult())));
+
+
+        resultActions.andDo(document("create-ad",
+                links(halLinks(),
+                        linkWithRel("curies").description("CUR-ies"),
+                        linkWithRel("self").description("This script"),
+                        linkWithRel("api:script").description("This <<scripts, script>>"),
+                        linkWithRel("api:perform").description("Running the script via PUT"),
+                        linkWithRel("api:view").description("View detail the script via PUT"),
+                        linkWithRel("api:terminate").description("Termination of the script via PUT with empty body")
+                ),
+                responseFields(
+                        fieldWithPath("_links").type(JsonFieldType.OBJECT).description("Links"),
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("Unique script id"),
+                        fieldWithPath("script").type(JsonFieldType.STRING).description("the body of the script"),
+                        fieldWithPath("status").type(JsonFieldType.STRING).description("Formal script status, one of " +
+                                Stream.of(Script.Status.values()).map(Enum::name).collect(Collectors.joining(", "))),
+                        fieldWithPath("result").type(JsonFieldType.STRING).description("Result of running of the script")
+
+                )));
+    }
 
     @Test
     public void setScriptById() throws Exception {
@@ -49,7 +115,7 @@ public class ScriptResourceControllerTest {
     @Test
     public void perform() throws Exception {
 
-        Script script = new Script();
+        Script script = script();//new Script();
         script.setScript("print('task1')");
         given(this.scriptProccessingService.perform(1L)).willReturn(script);
         mockMvc.perform(put("/scripts/" + 1L + "/running")
@@ -74,7 +140,7 @@ public class ScriptResourceControllerTest {
 //          script.setId(1L);
           script.setScript("while(true)");
           script.setStatus(Script.Status.NEW);
-          script.setResult("task1: ");
+          script.setResult("task1: Some result");
           return script;
       }
 
@@ -87,8 +153,8 @@ public class ScriptResourceControllerTest {
                 "}";
     }
 
-    private Script findRunningScript(){
-        return null;
+    private Script findCreatedScript(){
+        return scriptRepository.findAll(new Sort(Sort.Direction.DESC, "id")).iterator().next();
     }
 
 }
